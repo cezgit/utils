fdfbc492777b2413943a6dfa50ea36e5c6b5338a
Cezar <cezgit@gmail.com>
2020-05-31 22:02:30 -0700

    CollectionUtils fix for the get key from map for value utility

--- util/src/main/java/com/wds/util/CollectionUtils.java
+++ util/src/main/java/com/wds/util/CollectionUtils.java
@@ -56,11 +56,11 @@ public class CollectionUtils {
      * @param <V>
      * @return the key corresponding to the value arg
      */
-    public static <K, V> K getMapKey(Map<K, V> map, V value) {
+    public static <K, V> Optional<K> getMapKey(Map<K, V> map, V value) {
         return map.entrySet()
                 .stream()
                 .filter(entry -> value.equals(entry.getValue()))
                 .map(Map.Entry::getKey)
-                .findFirst().get();
+                .findFirst();
     }
 }
