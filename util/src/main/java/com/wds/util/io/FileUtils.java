12bb26955b2cefa7f8153280104da28b7a31f8a5
Cezar <cezgit@gmail.com>
2021-03-08 09:02:08 -0800

    fileutils: new method for writing to a file

--- util/src/main/java/com/wds/util/io/FileUtils.java
+++ util/src/main/java/com/wds/util/io/FileUtils.java
@@ -1,8 +1,13 @@
 package com.wds.util.io;
 
+import com.wds.util.date.DateUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.zeroturnaround.zip.ZipUtil;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,10 +17,14 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.lang.String.format;
+
 /**
  * more here: https://www.baeldung.com/reading-file-in-java
  */
 public class FileUtils {
+
+    private static Logger logger = LogManager.getLogger(FileUtils.class);
 
     public static List<String> readFileLines(String filePath) throws IOException {
         List<String> result;
@@ -23,6 +32,18 @@ public class FileUtils {
             result = lines.collect(Collectors.toList());
         }
         return result;
+    }
+
+    public static void writeToFile(List<String> lines, String fileName) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for (String line : lines) {
+                writer.write(line+"\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            logger.error(format("writing to %s failed with exception: %s", fileName, e.getMessage()));
+        }
     }
 
     public static BiConsumer<File, File> fileMover = (dirSrc, dirTarget) -> {
